df373deeb5e3e8bd06b47c830e15b46b63771780
algoksxun <130209866+algoksxun@users.noreply.github.com>
2023-07-03 18:21:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello world!')
+from telebot.async_telebot import AsyncTeleBot
+from random  import*
+
+bot = AsyncTeleBot("6303803551:AAHSolp6PGR07ZlGdPadSdDCwVVvP0jmbwY")
+@bot.message_handler(commands=['help','start'])
+async def send_welcom(message):
+    await bot.reply_to(message, 'приветствую тебя пользователь ')
+@bot.message_handler(commands=['Давайте поговорим?'])
+async def send_welcom(message):
+    await bot.reply_to(message, 'давайте , можете задать мне вопрос')
+@bot.message_handler(func=lambda message:True)
+async def  echo_message(messega):
+    text_message = messega.text
+    text_message = messega.text
+    if 'дела' in text_message or 'настроение' in text_message:
+        await  bot.reply_to((messega , 'все круто , а у тебя?'))
+    #if 'шутка' in text_message or ''
+
+import asyncio
+asyncio.run(bot.polling())
